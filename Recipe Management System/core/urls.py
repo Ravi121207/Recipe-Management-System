d5c9974181f065d4839d1c76577c6b61441e988b
Ravi121207 <ravikumarvip9528@gmail.com>
2025-12-15 00:23:45 +0530

    Update urls.py

--- Recipe Management System/core/urls.py
+++ Recipe Management System/core/urls.py
@@ -31,26 +31,9 @@ from counter import views  # make sure this is correct
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("graph/", views.graph, name="graph"),
-    path("front/", views.front, name="front"),
-    path("home_page/", views.home_page, name="home_page"),
-    path("dashboard_page/", views.dashboard_page, name="dashboard_page"),
-    path("register_page/", views.register_page, name="register_page"),
-    path("login_page/", views.login_page, name="login_page"),
-    path("video_feed/", views.video_feed, name="video_feed"),
-    path('logout/',views.logout_view,name='logout'),
-    
     path('receipes/',receipes,name="receipes"),
     path('delete-receipe/<id>/',delete_receipe,name="delete_receipe"),
     path('update-receipe/<id>/',update_receipe,name="update_receipe"),
-    # path('logins/',logins,name="logins"),
-    # path('logins/',logins, name='logins'),
-    path('forgot/',forgot,name="forgot"),
-    path('sign/',sign,name="sign"),
-    
-    # path('register/',register,name="register"),       
-    # path('login/',login,name=login),
-    
     path('login_view/',login_view,name="login_view"),
     path('register/',register,name="register"),
     path('logout_view/',logout_view,name="logout_view"),
@@ -60,52 +43,9 @@ urlpatterns = [
 
 
 
-
-
-
-# urlpatterns = [
-#     # path('',home,name="home"),
-#     path('receipes/',receipes,name="receipes"),
-#     path('delete-receipe/<id>/',delete_receipe,name="delete_receipe"),
-#     path('update-receipe/<id>/',update_receipe,name="update_receipe"),
-#     # path('logins/',logins,name="logins"),
-#     path('logins/',logins, name='logins'),
-#     path('forgot/',forgot,name="forgot"),
-#     path('sign/',sign,name="sign"),
-    
-#     path('about/',about,name="about"),
-#     path('contact/',contact,name="contact"),
-#     path('success-page/',success_page,name="success_page"),
-#     path('ravi/', admin.site.urls),
-#     path('login/',login_page,name="login_page"),
-#     path('register/',register,name="register"),
-    
-#     path('register_page/',register_page,name="register_page"),
-#     path('login_page/',login_page,name=login_page),
-    
-# ]
-
-# from django.contrib import admin
-# from django.urls import path
-# from counter import views
-
-# urlpatterns = [
-#     path("admin/", admin.site.urls),
-#     # path('register_page/',views.register_page,name="register_page"),
-#     # path('login_page/',views.login_page,name=login_page),
-#     path("home_page/",views.home_page, name="home_page"),
-#     # path('', views.home, name="home"),
-#     path('register_page/',views.register_page, name="register_page"),
-#     path('login_page/', views.login_page, name="login_page"),
-#     path('dashboard_page/', views.dashboard_page, name="dashboard_page"),
-#     path("video_feed/",views.video_feed, name="video_feed"),
-# ]
-
-
-
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
     
 urlpatterns += staticfiles_urlpatterns()
+
